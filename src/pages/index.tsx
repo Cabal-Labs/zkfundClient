6801114b43d2ffda44c8d1ab63608d05d5c8f197
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-02-01 13:08:50 -0500

    landing page and icon component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,101 @@ import Head from "next/head";
 import Image from "next/image";
 import { Button } from "@chakra-ui/react";
 import ScreenWrapper from "@/components/layout/screenWrapper";
+import Icon from "@/lib/icons";
 
 export default function Home() {
 	return (
-		<ScreenWrapper>
+		<ScreenWrapper title={"Welcome to zk.fund"} className="landing-page">
 			<main>
-				<div>
-					<h1>Welcome to zk.fund</h1>
-					<h2>Welcome to zk.fund</h2>
-					<h3>Welcome to zk.fund</h3>
-					<h4>Welcome to zk.fund</h4>
-					<h5>Welcome to zk.fund</h5>
-					<h6>Welcome to zk.fund</h6>
-					<p>paragraph</p>
-					<a href="">Link</a>
-					<div>
-						<Button variant={"contained"}>Get Started</Button>
+				<div id="landing">
+					<div className="title">
+						<h1>Donate Anonymously</h1>
+						<h3>powered by Aztec</h3>
 					</div>
-					<div>
-						<Button variant={"outlined"}>Get Started</Button>
+					<div className="cta">
+						<Button size={"lg"} variant={"contained"}>
+							Connect Wallet
+						</Button>
+						<a href="">What's a Wallet?</a>
 					</div>
-					<div>
-						<Button variant={"filled"}>Get Started</Button>
+					<div className="footer">
+						<Icon icon={"ChevronDown"} size={60} />
+						<h4>Learn More</h4>
 					</div>
 				</div>
+
+				<section className="" id="info-1">
+					<div className="content">
+						<h2>Why do we exist?</h2>
+						<p>
+							Laboris nostrud dolore proident culpa non mollit proident
+							reprehenderit aliqua et quis exercitation Lorem. Deserunt sunt
+							commodo elit fugiat fugiat ad proident magna. Incididunt do minim
+							irure veniam esse. Sint eiusmod do proident fugiat irure laborum
+							consequat pariatur reprehenderit velit minim cupidatat. Laborum
+							nulla aute reprehenderit excepteur nostrud velit reprehenderit
+							irure aliquip ad ea incididunt ipsum. Commodo cupidatat laborum
+							qui reprehenderit laboris non nulla commodo officia minim anim in
+							occaecat.
+						</p>
+					</div>
+					<div className="img-wrapper">
+						<Image
+							src={"https://picsum.photos/200"}
+							layout={"fill"}
+							objectFit={"cover"}
+							objectPosition={"center"}
+						/>
+					</div>
+				</section>
+				<section className="reverse" id="info-1">
+					<div className="content">
+						<h2>Donations are protected by Free Speech</h2>
+						<p>
+							Laboris nostrud dolore proident culpa non mollit proident
+							reprehenderit aliqua et quis exercitation Lorem. Deserunt sunt
+							commodo elit fugiat fugiat ad proident magna. Incididunt do minim
+							irure veniam esse. Sint eiusmod do proident fugiat irure laborum
+							consequat pariatur reprehenderit velit minim cupidatat. Laborum
+							nulla aute reprehenderit excepteur nostrud velit reprehenderit
+							irure aliquip ad ea incididunt ipsum. Commodo cupidatat laborum
+							qui reprehenderit laboris non nulla commodo officia minim anim in
+							occaecat.
+						</p>
+					</div>
+					<div className="img-wrapper">
+						<Image
+							src={"https://picsum.photos/200"}
+							layout={"fill"}
+							objectFit={"cover"}
+							objectPosition={"center"}
+						/>
+					</div>
+				</section>
+				<section className="" id="info-1">
+					<div className="content">
+						<h2>Verified charities give you peace of mind</h2>
+						<p>
+							Laboris nostrud dolore proident culpa non mollit proident
+							reprehenderit aliqua et quis exercitation Lorem. Deserunt sunt
+							commodo elit fugiat fugiat ad proident magna. Incididunt do minim
+							irure veniam esse. Sint eiusmod do proident fugiat irure laborum
+							consequat pariatur reprehenderit velit minim cupidatat. Laborum
+							nulla aute reprehenderit excepteur nostrud velit reprehenderit
+							irure aliquip ad ea incididunt ipsum. Commodo cupidatat laborum
+							qui reprehenderit laboris non nulla commodo officia minim anim in
+							occaecat.
+						</p>
+					</div>
+					<div className="img-wrapper">
+						<Image
+							src={"https://picsum.photos/200"}
+							layout={"fill"}
+							objectFit={"cover"}
+							objectPosition={"center"}
+						/>
+					</div>
+				</section>
 			</main>
 		</ScreenWrapper>
 	);
