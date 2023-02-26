bd28ece18d40e659f18f673c7702f615d15e862f
Amit Bhatnagar <abhatna7@stevens.edu>
2023-02-26 11:03:58 -0500

    renamed index for donate page, began components on donate page

---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,38 @@
+import ScreenWrapper from "@/components/layout/screenWrapper";
+import { Input } from "@chakra-ui/react";
+
+export default function Donate(props) {
+  return (
+    <ScreenWrapper className="home-page" title={"zk.fund Home"}>
+      <main>
+        <div className="container">
+          <div id="pick-charity">
+            {/* show selected charity, onclick opens modal */}
+            <img
+              id="charity-logo"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzmiAMnhX5wmE3rsbFP1Y6gIxdvTUxbC6sNhhNQnzqM9JEOtfWpjQuPHNj4luUVwILKr4&usqp=CAU"
+              alt="charity logo"
+            />
+            <h2>Name of charity</h2>
+            <div id="selectValueRow">
+              <Input variant={"underlined"} />
+            </div>
+          </div>
+          <div id="pick-amount">
+            {/* interactive select amount component */}
+          </div>
+        </div>
+        <div className="shapes">
+          <div className="shape-0"></div>
+          <div className="shape-1"></div>
+          <div className="shape-2"></div>
+          <div className="shape-3"></div>
+          <div className="shape-4"></div>
+          <div className="shape-5"></div>
+          <div className="shape-6"></div>
+          <div className="shape-7"></div>
+        </div>
+      </main>
+    </ScreenWrapper>
+  );
+}
--- src/styles/chakraTheme.ts
+++ src/styles/chakraTheme.ts
@@ -1,235 +1,248 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const config = {
-	initialColorMode: "dark",
-	useSystemColorMode: false,
+  initialColorMode: "dark",
+  useSystemColorMode: false,
 };
 const colors = {
-	primary: "#D37CF1",
-	primaryLight: "#E5A7FB",
-	primaryDark: "#961BC0",
-	text: "#FCFCFC",
-	secondaryText: "#cccccc",
-	border: "#333333",
-	background: "#22142F",
-	foreground: "#422552",
-	danger: "#C74F4F",
-	success: "#65B36D",
-	link: "#4C81BF",
-	buttonBackground: "#2B242E",
+  primary: "#D37CF1",
+  primaryLight: "#E5A7FB",
+  primaryDark: "#961BC0",
+  text: "#FCFCFC",
+  secondaryText: "#cccccc",
+  border: "#333333",
+  background: "#22142F",
+  foreground: "#422552",
+  danger: "#C74F4F",
+  success: "#65B36D",
+  link: "#4C81BF",
+  buttonBackground: "#2B242E",
 };
 const components = {
-	Button: {
-		baseStyle: {
-			margin: 0.5,
-			_hover: { opacity: "85%" },
-			_focus: {
-				boxShadow: "none",
-			},
-		},
-		sizes: {
-			xl: {
-				h: "56px",
-				fontSize: "lg",
-				px: "32px",
-			},
-		},
+  Button: {
+    baseStyle: {
+      margin: 0.5,
+      _hover: { opacity: "85%" },
+      _focus: {
+        boxShadow: "none",
+      },
+    },
+    sizes: {
+      xl: {
+        h: "56px",
+        fontSize: "lg",
+        px: "32px",
+      },
+    },
 
-		variants: {
-			contained: () => ({
-				color: "white",
-				backgroundColor: colors.primary,
-				_hover: {
-					outlineColor: colors.primaryDark,
-					outlineWidth: 1,
-					outlineStyle: "solid",
-				},
-				_disabled: {
-					opacity: 0.25,
-					backgroundColor: colors.primary,
-				},
-			}),
-			primary: () => ({
-				color: colors.primaryDark,
-				backgroundColor: colors.primaryLight,
-			}),
-			danger: () => ({
-				color: "white",
-				backgroundColor: colors.danger,
-			}),
-			success: () => ({
-				color: "white",
-				backgroundColor: colors.success,
-			}),
-			outlined: () => ({
-				borderWidth: 1,
-				borderStyle: "solid",
-				borderColor: colors.secondaryText,
-				color: colors.text,
-				_hover: { backgroundColor: colors.foreground },
-			}),
-			pinkOutline: () => ({
-				backgroundColor: colors.buttonBackground,
-				borderRadius: 1000,
-				borderWidth: 2,
-				borderColor: colors.primary,
-				paddingLeft: 10,
-				paddingRight: 10,
-				paddingTop: 5,
-				paddingBottom: 5,
-				fontFamily: "Arvo",
-				fontWeight: "regular",
-				fontSize: "lg",
-			}),
-			text: () => ({
-				_hover: { backgroundColor: colors.foreground },
-			}),
-			link: () => ({
-				color: colors.link,
-				_hover: { backgroundColor: colors.foreground },
-			}),
-			filled: () => ({
-				backgroundColor: colors.foreground,
-			}),
-		},
-		defaultProps: {
-			variant: "contained",
-			size: "md",
-		},
-	},
-	IconButton: {
-		variant: {
-			text: () => ({
-				backgroundColor: "transparent",
-			}),
-		},
-	},
-	Tooltip: {
-		baseStyle: () => ({
-			borderRadius: 5,
-			backgroundColor: colors.foreground,
-			color: colors.text,
-		}),
-	},
+    variants: {
+      contained: () => ({
+        color: "white",
+        backgroundColor: colors.primary,
+        _hover: {
+          outlineColor: colors.primaryDark,
+          outlineWidth: 1,
+          outlineStyle: "solid",
+        },
+        _disabled: {
+          opacity: 0.25,
+          backgroundColor: colors.primary,
+        },
+      }),
+      primary: () => ({
+        color: colors.primaryDark,
+        backgroundColor: colors.primaryLight,
+      }),
+      danger: () => ({
+        color: "white",
+        backgroundColor: colors.danger,
+      }),
+      success: () => ({
+        color: "white",
+        backgroundColor: colors.success,
+      }),
+      outlined: () => ({
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: colors.secondaryText,
+        color: colors.text,
+        _hover: { backgroundColor: colors.foreground },
+      }),
+      pinkOutline: () => ({
+        backgroundColor: colors.buttonBackground,
+        borderRadius: 1000,
+        borderWidth: 2,
+        borderColor: colors.primary,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        fontFamily: "Arvo",
+        fontWeight: "regular",
+        fontSize: "lg",
+      }),
+      text: () => ({
+        _hover: { backgroundColor: colors.foreground },
+      }),
+      link: () => ({
+        color: colors.link,
+        _hover: { backgroundColor: colors.foreground },
+      }),
+      filled: () => ({
+        backgroundColor: colors.foreground,
+      }),
+    },
+    defaultProps: {
+      variant: "contained",
+      size: "md",
+    },
+  },
+  IconButton: {
+    variant: {
+      text: () => ({
+        backgroundColor: "transparent",
+      }),
+    },
+  },
+  Tooltip: {
+    baseStyle: () => ({
+      borderRadius: 5,
+      backgroundColor: colors.foreground,
+      color: colors.text,
+    }),
+  },
 
-	Input: {
-		baseStyle: {},
-		variants: {
-			outlined: () => ({
-				field: {
-					background: "transparent",
-					borderWidth: 2,
-					borderColor: colors.border,
-					_focus: {
-						borderColor: colors.primary,
-						borderWidth: 2,
-					},
-				},
-			}),
-			filled: () => ({
-				backgroundColor: colors.foreground,
-				_focus: {
-					borderColor: colors.primary,
-					borderWidth: 2,
-				},
-			}),
-		},
-		defaultProps: { variant: "outlined" },
-	},
-	Textarea: {
-		baseStyle: {},
-		variants: {
-			outlined: () => ({
-				background: "transparent",
-				borderWidth: 2,
-				borderColor: colors.border,
-				_focus: {
-					borderColor: colors.primary,
-					borderWidth: 2,
-				},
-			}),
-		},
-		defaultProps: { variant: "outlined" },
-	},
-	Select: {
-		baseStyle: {},
-		variants: {
-			outlined: () => ({
-				field: {
-					background: "transparent",
-					borderWidth: 2,
-					borderColor: colors.border,
-					_focus: {
-						borderColor: colors.primary,
-						borderWidth: 2,
-					},
-				},
-			}),
-		},
-		defaultProps: { variant: "outlined" },
-	},
-	FormLabel: {
-		baseStyle: {
-			fontFamily: "Arvo",
-			fontSize: "lg",
-		},
-	},
-	Modal: {
-		parts: ["dialog", "header", "body"],
-		variants: {
-			outlined: () => ({
-				dialog: {
-					backgroundColor: colors.buttonBackground,
-					borderRadius: 15,
-					borderWidth: 2,
-					borderColor: colors.primary,
-				},
-			}),
-		},
-		defaultProps: { variant: "outlined" },
-	},
-	Popover: {
-		variants: {
-			primary: () => ({
-				backgroundColor: colors.foreground,
-			}),
-		},
-	},
-	Tag: {
-		variants: {
-			contained: () => ({
-				color: "white",
-				backgroundColor: colors.primary,
-			}),
-			danger: () => ({
-				color: "white",
-				backgroundColor: colors.danger,
-			}),
-			outlined: () => ({
-				borderWidth: 1,
-				borderStyle: "solid",
-				borderColor: colors.secondaryText,
-				color: colors.text,
-				_hover: { backgroundColor: colors.foreground },
-			}),
-			text: () => ({
-				_hover: { backgroundColor: colors.foreground },
-			}),
-			filled: () => ({
-				backgroundColor: colors.foreground,
-			}),
-		},
-	},
-	Drawer: {
-		parts: ["dialog", "header", "body"],
-		variants: {
-			primary: () => ({
-				dialog: {
-					backgroundColor: colors.foreground,
-				},
-			}),
-		},
-	},
+  Input: {
+    baseStyle: {},
+    variants: {
+      outlined: () => ({
+        field: {
+          background: "transparent",
+          borderWidth: 2,
+          borderColor: colors.border,
+          _focus: {
+            borderColor: colors.primary,
+            borderWidth: 2,
+          },
+        },
+      }),
+      filled: () => ({
+        backgroundColor: colors.foreground,
+        _focus: {
+          borderColor: colors.primary,
+          borderWidth: 2,
+        },
+      }),
+      underlined: () => ({
+        field: {
+          backgroundColor: "transparent",
+          borderBottomWidth: 3,
+		  borderRadius: 0,
+		  borderColor: "black",
+		  _focus: {
+			borderColor: colors.primary,
+		  },
+		  fontSize: 40,
+		  padding: "2rem",
+        },
+      }),
+    },
+    defaultProps: { variant: "outlined" },
+  },
+  Textarea: {
+    baseStyle: {},
+    variants: {
+      outlined: () => ({
+        background: "transparent",
+        borderWidth: 2,
+        borderColor: colors.border,
+        _focus: {
+          borderColor: colors.primary,
+          borderWidth: 2,
+        },
+      }),
+    },
+    defaultProps: { variant: "outlined" },
+  },
+  Select: {
+    baseStyle: {},
+    variants: {
+      outlined: () => ({
+        field: {
+          background: "transparent",
+          borderWidth: 2,
+          borderColor: colors.border,
+          _focus: {
+            borderColor: colors.primary,
+            borderWidth: 2,
+          },
+        },
+      }),
+    },
+    defaultProps: { variant: "outlined" },
+  },
+  FormLabel: {
+    baseStyle: {
+      fontFamily: "Arvo",
+      fontSize: "lg",
+    },
+  },
+  Modal: {
+    parts: ["dialog", "header", "body"],
+    variants: {
+      outlined: () => ({
+        dialog: {
+          backgroundColor: colors.buttonBackground,
+          borderRadius: 15,
+          borderWidth: 2,
+          borderColor: colors.primary,
+        },
+      }),
+    },
+    defaultProps: { variant: "outlined" },
+  },
+  Popover: {
+    variants: {
+      primary: () => ({
+        backgroundColor: colors.foreground,
+      }),
+    },
+  },
+  Tag: {
+    variants: {
+      contained: () => ({
+        color: "white",
+        backgroundColor: colors.primary,
+      }),
+      danger: () => ({
+        color: "white",
+        backgroundColor: colors.danger,
+      }),
+      outlined: () => ({
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: colors.secondaryText,
+        color: colors.text,
+        _hover: { backgroundColor: colors.foreground },
+      }),
+      text: () => ({
+        _hover: { backgroundColor: colors.foreground },
+      }),
+      filled: () => ({
+        backgroundColor: colors.foreground,
+      }),
+    },
+  },
+  Drawer: {
+    parts: ["dialog", "header", "body"],
+    variants: {
+      primary: () => ({
+        dialog: {
+          backgroundColor: colors.foreground,
+        },
+      }),
+    },
+  },
 };
 const chakraTheme = extendTheme({ components, colors, config });
 export default chakraTheme;
